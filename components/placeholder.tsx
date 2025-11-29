245df80cfdc4da0679169b4fda9623d36f21d574
flozdawg <fsd114@posteo.de>
2025-11-29 18:09:44 +0100

    added dynamic placeholder with optional attributes for tickets.id slug

---
+++ components/placeholder.tsx
@@ -0,0 +1,28 @@
+import { LucideMessageSquareWarning } from "lucide-react";
+import { cloneElement } from "react";
+import { JSX } from "react";
+
+type PlaceholderProps = {
+  label: string;
+  icon?: JSX.Element;
+  button?: JSX.Element | null;
+};
+
+const Placeholder = ({
+  label,
+  icon = <LucideMessageSquareWarning />,
+  button = <div className="h-10" />,
+}: PlaceholderProps) => {
+  return (
+    <div className="flex-1 self-center flex flex-col items-center justify-center gap-y-2">
+      {cloneElement(icon, {
+        className: "w-16 h-16",
+      })}
+
+      <h2 className="text-lg text-center">{label}</h2>
+      {button}
+    </div>
+  );
+};
+
+export { Placeholder };
