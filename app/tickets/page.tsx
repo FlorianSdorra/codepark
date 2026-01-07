b23abe4e9e2ca274bd948390abfea492f48a3284
flozdawg <fsd114@posteo.de>
2026-01-07 16:17:25 +0100

    change to fetching ticket data with hooks and artificial delay in TicketsPage

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,13 +1,28 @@
-import initialTickets from "@/data";
+"use client";
+
+import { useEffect, useState } from "react";
 import { Heading } from "@/components/heading";
 import { TicketItem } from "@/app/features/ticket/components/ticket-item";
+import { Ticket } from "../features/ticket/types";
+import { getTickets } from "../features/ticket/queries/get-tickets";
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const result = await getTickets();
+      setTickets(result);
+    };
+
+    fetchTickets();
+  });
+
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {initialTickets.map((ticket) => {
+        {tickets.map((ticket) => {
           return <TicketItem key={ticket.id} ticket={ticket} />;
         })}
       </div>
