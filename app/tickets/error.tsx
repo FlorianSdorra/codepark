b67e5f209ef6ddac600a9da9711b95501c2263b9
flozdawg <fsd114@posteo.de>
2026-01-08 17:16:51 +0100

    added error route for error boundary fallback

---
+++ app/tickets/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Placeholder } from "@/components/placeholder";
+
+export default function Error({ error }: { error: Error }) {
+  return (
+    <Placeholder
+      label={error.message || "An unexpected error occurred."}
+    ></Placeholder>
+  );
+}
