89bf9067a09ba1db1533e2339a622fb3de3b84d9
flozdawg <fsd114@posteo.de>
2026-01-12 16:16:10 +0100

    change ticket queries to prisma client

--- app/features/ticket/queries/get-tickets.ts
+++ app/features/ticket/queries/get-tickets.ts
@@ -1,12 +1,7 @@
-import initialTickets from "@/data";
-import { Ticket } from "../types";
+import { prisma } from "../../../../lib/prisma";
 
-export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // artificial delay
-
-  // throw new Error("Failed to fetch tickets"); // Simulate an error for demonstration
-
-  return new Promise((resolve) => {
-    resolve(initialTickets);
+export const getTickets = async () => {
+  return await prisma.ticket.findMany({
+    orderBy: { createdAt: "desc" },
   });
 };
