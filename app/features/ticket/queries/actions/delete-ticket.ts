11ac76213739d7a8613bb1b422172e5d8d3d2875
flozdawg <fsd114@posteo.de>
2026-01-15 18:57:47 +0100

    change from time based ISR to on demand based ISR to checkout differnces in prod

--- app/features/ticket/queries/actions/delete-ticket.ts
+++ app/features/ticket/queries/actions/delete-ticket.ts
@@ -3,13 +3,15 @@
 import { redirect } from "next/navigation";
 import { prisma } from "../../../../../lib/prisma";
 import { ticketsPath } from "@/paths";
+import { revalidatePath } from "next/cache";
 
 export const deleteTicket = async (ticketId: string) => {
   await prisma.ticket.delete({
     where: { id: ticketId },
   });
-
   // TODO: Add flash message for successful deletion
+
+  revalidatePath(ticketsPath());
 
   redirect(ticketsPath());
 };
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,8 +3,6 @@ import { Heading } from "@/components/heading";
 import { TicketList } from "../features/ticket/components/ticket-list";
 import { Spinner } from "@/components/spinner";
 
-export const revalidate = 30;
-
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
