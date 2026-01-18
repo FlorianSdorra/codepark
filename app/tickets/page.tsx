924677266e996b07f42780ac37d961de27926c77
flozdawg <fsd114@posteo.de>
2026-01-18 22:33:04 +0100

    add reusable CardComponent

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -2,28 +2,18 @@ import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 import { TicketList } from "../features/ticket/components/ticket-list";
 import { Spinner } from "@/components/spinner";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { CardCompact } from "@/components/card-compact";
 import TicketCreateForm from "../features/ticket/components/ticket-create-form";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-      <Card className="w-full max-w-[420px] self-center">
-        <CardHeader>
-          <CardTitle>Create Ticket</CardTitle>
-          <CardDescription>A new Ticket will be created</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <TicketCreateForm />
-        </CardContent>
-      </Card>
+      <CardCompact
+        title="Create Ticket"
+        description="A new Ticket will be created"
+        content={<TicketCreateForm />}
+      />
       <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
