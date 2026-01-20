708644b8dc9e50aed382ef45b006b1a7bda9bf0d
flozdawg <fsd114@posteo.de>
2026-01-20 20:30:08 +0100

    add useTransform hook in upsert form to have loading spinner in button

--- app/features/ticket/components/ticket-upsert-form.tsx
+++ app/features/ticket/components/ticket-upsert-form.tsx
@@ -1,27 +1,41 @@
+"use client";
+
 import { Ticket } from "@/lib/generated/prisma/client";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { upsertTicket } from "../queries/actions/upsert-ticket";
+import { useTransition } from "react";
+import { LucideLoaderCircle } from "lucide-react";
 
 type TicketUpsertFormProps = {
   ticket?: Ticket;
 };
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const upsertTicketAction = (formData: FormData) => {
+    startTransition(async () => {
+      await upsertTicket.bind(null, ticket?.id)(formData);
+    });
+  };
+
   return (
-    <form
-      action={upsertTicket.bind(null, ticket?.id)}
-      className="flex flex-col gap-y-2 w-full"
-    >
+    <form action={upsertTicketAction} className="flex flex-col gap-y-2 w-full">
       <Label htmlFor="title">Title</Label>
       <Input id="title" name="title" type="text" defaultValue={ticket?.title} />
 
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-      <Button type="submit">{ticket ? "Edit" : "Create"}</Button>
+      <Button type="submit">
+        {ticket ? "Edit" : "Create"}
+        {isPending && (
+          <LucideLoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+        )}
+      </Button>
     </form>
   );
 };
