41ba2a06864a98a35c99617308cdc0a04c558ed1
flozdawg <fsd114@posteo.de>
2026-01-21 19:26:14 +0100

    add try & catch to upsert ticket action with form validation and sending error to client

--- app/features/ticket/queries/actions/upsert-ticket.ts
+++ app/features/ticket/queries/actions/upsert-ticket.ts
@@ -4,24 +4,34 @@ import { redirect } from "next/navigation";
 import { prisma } from "../../../../../lib/prisma";
 import { ticketPath, ticketsPath } from "@/paths";
 import { revalidatePath } from "next/cache";
+import z from "zod";
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(100),
+  content: z.string().min(1).max(1000),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
-
-  await prisma.ticket.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data,
-  });
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title") as string,
+      content: formData.get("content") as string,
+    });
+    await prisma.ticket.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { message: "Failed to create or update the ticket." };
+  }
 
   revalidatePath(ticketsPath());
 
